c27767dd120c7be52677d47046416e99aea7070c
yannisl <y.lazarides@habtoorspecon.com>
2014-12-26 19:36:32 +0400

    major changes

---
+++ logging.lua
@@ -0,0 +1,4 @@
+--if type(tex)=='table' then local print=tex.print end
+local m = m or {}
+
+return tex.print('in local folder')
---
+++ readme.md
@@ -0,0 +1,3 @@
+*Bullet Graphs*
+
+This is a short demonstration of using LuaTeX to generate bullet graphs.
